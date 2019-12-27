7a42d0b056a41a124412cdac4404c8488ec0cd8c
alexglzg <alex_gg97@hotmail.com>
2019-12-27 06:24:01 -0600

    auto_nav update

--- scripts/auto_nav_position.py
+++ scripts/auto_nav_position.py
@@ -19,11 +19,11 @@ class AutoNav:
         self.obj_list = []
         self.activated = True
         self.state = -1
-        self.ang = 0
-        self.desired_speed = 0
         self.distance = 0
+        self.xm = 0
+        self.ym = 0
         self.InitTime = rospy.Time.now().secs
-        self.desired_heading = 0
+        self.offset = .55 #camera to ins offset
 
         rospy.Subscriber("/vectornav/ins_2d/ins_pose", Pose2D, self.ins_pose_callback)
         rospy.Subscriber('/usv_perception/yolo_zed/objects_detected', ObjDetectedList, self.objs_callback)
@@ -43,39 +43,64 @@ class AutoNav:
 
     def punto_medio(self):
 
-        distances_list = []
+        x_list = []
         y_list = []
         class_list = []
+        distance_list = []
         for i in range(len(self.obj_list)):
-            distances_list.append(self.obj_list[i]['X'])
+            x_list.append(self.obj_list[i]['X'])
             y_list.append(self.obj_list[i]['Y'])
             class_list.append(self.obj_list[i]['class'])
+            distance_list.append(math.pow(x_list[i]**2 + y_list[i]**2, 0.5))
 
-        ind_x1 = np.argsort(distances_list)[0]
-        ind_x2 = np.argsort(distances_list)[1]
+        ind_x1 = np.argsort(distance_list)[0]
+        ind_x2 = np.argsort(distance_list)[1]
 
-        x1 = distances_list[ind_x1]
+        x1 = x_list[ind_x1]
         y1 = -1*y_list[ind_x1]
-        x2 = distances_list[ind_x2]
+        x2 = x_list[ind_x2],,,,,,,,,,
         y2 = -1*y_list[ind_x2]
         xc = min([x1,x2]) + abs(x1 - x2)/2
         yc = min([y1,y2]) + abs(y1 - y2)/2
-        self.distance = xc
-        offset = .55
+
+        self.distance = math.pow(xc**2 + yc**2, 0.5)
+
         yc = 0.00001 if yc == 0 else yc
 
-        relative_heading = math.atan((xc+offset)/yc)
+        relative_angle = math.atan(xc/yc)
+
+        
 
         obj = Float32MultiArray()
         obj.layout.data_offset = 5
-        obj.data = [xc,yc,xm,ym,2]
+        obj.data = [xc,yc,self.xm,self.ym,2]
 
-        self.d_heading_pub.publish(heading)
+        self.path_pub.publish(obj)
+
+    '''def gate_to_body(self, x, y, angle):
+        p = np.array([[x],[y]])
+        J = np.array([[math.cos(angle), -1*math.sin(angle)],[math.sin(angle), math.cos(angle)]])
+        n = J.dot(p)
+
+        nedx = n[0] + self.NEDx
+        nedy = n[1] + self.NEDy
+
+        return (x_body, y_body)'''
+
+    def farther():
+        self.xm = self.xm + 2
+
+        obj = Float32MultiArray()
+        obj.layout.data_offset = 3
+        obj.data = [self.xm,self.ym,2]
+
+        self.path_pub.publish(obj)
 
 def main():
     rospy.init_node('auto_nav_position', anonymous=True)
     rate = rospy.Rate(100)
     E = AutoNav()
+    E.distance = 4
     while not rospy.is_shutdown() and E.activated:
 
         if E.state == -1:
@@ -84,38 +109,43 @@ def main():
                 rate.sleep()
             E.state = 0
 
-        if E.state == 0:
+        elif E.state == 0:
             E.test.publish(E.state)
-            if len(E.obj_list) >= 2:
+            if len(E.obj_list) >= 2 and E.distance >= 3:
                 E.punto_medio()
             else:
                 initTime = rospy.Time.now().secs
-                while not rospy.is_shutdown() and (len(E.obj_list) < 2 or E.distance < 2):
-                    if rospy.Time.now().secs - initTime > 5:
+                while not rospy.is_shutdown() and (len(E.obj_list) < 2 or E.distance < 3):
+                    if rospy.Time.now().secs - initTime > 3:
                         E.state = 1
                         rate.sleep()
                         break
 
-        if E.state == 1:
+        elif E.state == 1:
             E.test.publish(E.state)
             if len(E.obj_list) >= 2:
                 E.state = 2
             else:
-                E.farther(2)
+                initTime = rospy.Time.now().secs
+                while not rospy.is_shutdown() and (len(E.obj_list)) < 2:
+                    if rospy.Time.now().secs - initTime > 3:
+                        E.farther()
+                        initTime = rospy.Time.now().secs
+                        break
 
-        if E.state == 2:
+        elif E.state == 2:
             E.test.publish(E.state)
-            if len(E.obj_list) >= 2:
+            if len(E.obj_list) >= 2 and E.distance >= 3:
                 E.punto_medio()
             else:
                 initTime = rospy.Time.now().secs
-                while not rospy.is_shutdown() and (len(E.obj_list) < 2 or E.distance < 2):
-                    if rospy.Time.now().secs - initTime > 5:
+                while not rospy.is_shutdown() and (len(E.obj_list) < 2 or E.distance < 3):
+                    if rospy.Time.now().secs - initTime > 3:
                         E.state = 3
                         rate.sleep()
                         break
 
-        if E.state == 3:
+        elif E.state == 3:
             E.test.publish(E.state)
             time.sleep(1)
             E.status_pub.publish(1)
